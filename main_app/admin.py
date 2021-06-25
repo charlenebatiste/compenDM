f6862f0ba92f53d1d65c80352af51c970dae5156
charlenebatiste <batistecharlene@gmail.com>
2021-06-25 13:05:32 -0400

    added views, templates and urls for login and signup pages

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+# Import models here
+from .models import Journal
 # Register your models here.
+admin.site.register(Journal)
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
+# Function to render index page
 def index(request):
-  return render(request, 'index.html')
+    return render(request, 'index.html')
+
+#Function to render login page
+def login_view(request):
+# if post, then authenticate (user submitted username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/user/'+u)
+                else:
+                    print('The account has been disabled.')
+                    return HttpResponseRedirect('/')
+        else:
+            print('The username and/or password is incorrect.')
+            return HttpResponseRedirect('/login')
+    else: # it was a get request so send the emtpy login form
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+#function to render signup page
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
